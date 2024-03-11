27e53ff33e23f4b4dcd63950f3b783b35786812a
Guurf <102306125+Guurf@users.noreply.github.com>
2024-03-11 20:15:50 +0000

    lighting + mid way through sorting lol

--- 2D Platformerrr/Scenes/button.gd
+++ 2D Platformerrr/Scenes/button.gd
@@ -1,4 +1,7 @@
 extends Node2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
 var activated: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +13,7 @@ func _process(delta):
 	pass
 
 func _on_area_2d_area_entered(area):
+	animated_sprite_2d.frame = 1
 	activated = true
 	for child in get_children():
 		if child is TileMap:
